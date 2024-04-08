c125c971977a7e31fef0508ead18d5d54f5b7b01
Felipe Silva Ganho <felipeganho@gmail.com>
2024-04-07 22:09:09 -0300

    feat: new contents to use

--- kube-news/src/server.js
+++ kube-news/src/server.js
@@ -5,16 +5,22 @@ const bodyParser = require('body-parser')
 const promBundle = require("express-prom-bundle");
 const config = require('./system-life');
 const middlewares = require('./middleware')
+var multer = require('multer')
+const { v4: uuidv4 } = require('uuid');
+const { S3Client } = require('@aws-sdk/client-s3')
+const fs = require('fs');
+const multerS3 = require('multer-s3')
+const os = require('os')
 
 const metricsMiddleware = promBundle({
-    includeMethod: true, 
-    includePath: true, 
-    includeStatusCode: true, 
+    includeMethod: true,
+    includePath: true,
+    includeStatusCode: true,
     includeUp: true,
     promClient: {
         collectDefaultMetrics: {
         }
-      }
+    }
 });
 
 app.use(middlewares.countRequests)
@@ -26,16 +32,66 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 app.set('view engine', 'ejs');
 
+const AWS_ACCESS_KEY = process.env.AWS_ACCESS_KEY;
+const AWS_ACCESS_SECRET = process.env.AWS_ACCESS_SECRET;
+const AWS_REGION = process.env.AWS_REGION || "us-east-1";
+const AWS_S3_BUCKET_NAME = process.env.AWS_S3_BUCKET_NAME;
+const STORAGE_TYPE = process.env.STORAGE_TYPE || "LOCAL";
+
+let upload;
+
+if (STORAGE_TYPE === "LOCAL") {
+
+    const dirName = 'image_public/'
+
+    if (!fs.existsSync(dirName)) {
+        fs.mkdirSync(dirName);
+    }
+
+    const storage = multer.diskStorage({
+        destination: function (req, file, cb) {
+            cb(null, dirName)
+        },
+        filename: function (req, file, cb) {
+            cb(null, uuidv4() + '.jpg')
+        }
+    });
+
+    upload = multer({ storage: storage });
+} else {
+    // S3
+    const s3Client = new S3Client({
+        credentials: {
+            accessKeyId: AWS_ACCESS_KEY,
+            secretAccessKey: AWS_ACCESS_SECRET
+        }, region: AWS_REGION
+    })
+
+    upload = multer({
+        storage: multerS3({
+            s3: s3Client,
+            bucket: AWS_S3_BUCKET_NAME,
+            acl: 'public-read',
+            metadata: function (req, file, cb) {
+                cb(null, { fieldName: file.fieldname });
+            },
+            key: function (req, file, cb) {
+                cb(null, uuidv4() + '.jpg')
+            }
+        })
+    });
+}
 
 app.get('/post', (req, res) => {
-    res.render('edit-news', {post: {title: "", content: "", summary: ""}, valido: true});
+    res.render('edit-news', { post: { title: "", content: "", summary: "" }, valido: true });
 });
 
-app.post('/post', async (req, res) => {
+const cpUpload = upload.fields([{ name: 'sampleFile', maxCount: 1 }])
+app.post('/post', cpUpload, async (req, res) => {
 
     let valid = true;
 
-    if ((req.body.title.length !== 0 && req.body.title.length < 30) && 
+    if ((req.body.title.length !== 0 && req.body.title.length < 30) &&
         (req.body.resumo.length !== 0 && req.body.resumo.length < 50) &&
         (req.body.description.length !== 0 && req.body.description.length < 2000)) {
         valid = true;
@@ -44,37 +100,70 @@ app.post('/post', async (req, res) => {
     }
 
     if (valid) {
-        await models.Post.create({title: req.body.title, content: req.body.description, summary: req.body.resumo, publishDate: Date.now()});
+
+        let fileName;
+
+        if (!req.files || Object.keys(req.files).length === 0) {
+            return res.status(400).send('No files were uploaded.');
+        }
+
+        if (STORAGE_TYPE === "LOCAL") {
+            // Local
+            fileName = "/image/" + req.files['sampleFile'][0].filename;
+        } else {
+            // S3
+            fileName = req.files['sampleFile'][0].location;
+        }
+
+        await models.Post.create({
+            title: req.body.title,
+            content: req.body.description,
+            summary: req.body.resumo,
+            image: fileName,
+            publishDate: Date.now()
+        });
+
         res.redirect('/');
+
     } else {
-        res.render('edit-news', {post: {title: req.body.title, content: req.body.description, summary: req.body.resumo}, valido: false});
+        res.render('edit-news', { post: { title: req.body.title, content: req.body.description, summary: req.body.resumo }, valido: false });
     }
-    
+
 });
 
 app.post('/api/post', async (req, res) => {
 
     console.log(req.body.artigos)
-    for(const item of req.body.artigos) {
+    for (const item of req.body.artigos) {
 
-        await models.Post.create({title: item.title, content: item.description, summary: item.resumo, publishDate: Date.now()});
+        await models.Post.create({ title: item.title, content: item.description, summary: item.resumo, publishDate: Date.now() });
     }
 
-    // models.Post.create({title: req.body.title, content: req.body.description, summary: req.body.resumo, publishDate: Date.now()});
     res.json(req.body.artigos)
 });
 
 app.get('/post/:id', async (req, res) => {
 
     const post = await models.Post.findByPk(req.params.id);
-    res.render('view-news',{post: post});
+    res.render('view-news', { post: post });
 });
 
+app.get('/image/:imagem', async function (req, res) {
+
+    res.download('image_public/' + req.params.imagem)
+});
+
+app.get('/info', async (req, res) => {
+
+    const pjson = require('./package.json');
+    console.log(pjson.version);
+    res.render('info', { maquina: os.hostname, versao: pjson.version});
+});
 
 app.get('/', async (req, res) => {
 
     const posts = await models.Post.findAll();
-    res.render('index',{posts: posts});
+    res.render('index', { posts: posts });
 });
 
 models.initDatabase();
